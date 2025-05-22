8f84ec6e5e1aefbf0e07a013815333e64cd9e1f3
Arthur Gomes <juarhotmail295@gmail.com>
2025-05-22 13:30:59 -0300

    .

--- CasaMateriaisDeConstrucao/obj/Debug/net9.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
+++ CasaMateriaisDeConstrucao/obj/Debug/net9.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d29cf6986ec66002137fafa7c09fe4db148d50b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046fe27e82d0f0310621af955d9396a9d4bd8aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
